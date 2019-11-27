83428626ad8d02313384dd048616340d128ea5b9
tfyzxc13662049573 <420144542@qq.com>
2019-11-27 08:41:09 +0800

    Initial Commit

---
+++ TFY_TabBarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TFY_TabBarController
+//
+//  Created by tiandengyou on 2019/11/27.
+//  Copyright © 2019 田风有. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
