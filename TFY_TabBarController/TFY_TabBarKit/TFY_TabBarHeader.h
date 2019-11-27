dc4a65c8fbeeebe60c5b623ed15fe0d39545069c
tfyzxc13662049573 <420144542@qq.com>
2019-11-27 10:02:13 +0800

    youhua

--- TFY_TabBarController/TFY_TabBarKit/TFY_TabBarHeader.h
+++ TFY_TabBarController/TFY_TabBarKit/TFY_TabBarHeader.h
@@ -6,8 +6,22 @@
 //  Copyright © 2019 恋机科技. All rights reserved.
 //
 
-#ifndef TFY_TabBarHeader_h
-#define TFY_TabBarHeader_h
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+
+#define TFY_AutoLayoutKitRelease 0
+
+#if TFY_AutoLayoutKitRelease
+//自定义的
+#import <TFY_CustomizeTabBar/TFY_TabBarController.h>
+#import <TFY_CustomizeTabBar/TFY_TabContentView.h>
+//系统自带
+#import <TFY_SystemTabBar/TfySY_TabBarController.h>
+#import <TFY_SystemTabBar/TfySY_TestTabBar.h>
+
+#else
+
 //自定义的
 #import "TFY_TabBarController.h"
 #import "TFY_TabContentView.h"
@@ -15,4 +29,5 @@
 #import "TfySY_TabBarController.h"
 #import "TfySY_TestTabBar.h"
 
-#endif /* TFY_TabBarHeader_h */
+#endif
+
